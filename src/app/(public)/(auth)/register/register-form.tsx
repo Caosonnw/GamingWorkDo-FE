58a7310adde13995c46ae2f6a8ccfe2afb6f6f15
Tạ Lý Thuận <talythuan897@gmail.com>
2025-01-14 20:54:52 +0700

    New: Register

---
+++ src/app/(public)/(auth)/register/register-form.tsx
@@ -0,0 +1,195 @@
+'use client'
+
+import { path } from '@/common/path'
+import { DatePicker } from '@/components/date-picker'
+import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
+import { Form, FormField, FormItem, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+import { useAlert } from '@/context/AlertContext'
+import { RegisterBody, RegisterBodyType } from '@/schema/auth.schema'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Label } from '@radix-ui/react-label'
+import { format } from 'date-fns'
+import { useRouter } from 'next/navigation'
+import { useForm } from 'react-hook-form'
+
+export default function RegisterForm() {
+  const { showAlert } = useAlert()
+  const router = useRouter()
+
+  const form = useForm({
+    resolver: zodResolver(RegisterBody),
+    defaultValues: {
+      full_name: '',
+      email: '',
+      password: '',
+      gender: '1' as '1' | '0',
+      date_of_birth: undefined as string | undefined,
+      phone_number: undefined as string | undefined
+    }
+  })
+
+  const onSubmit = async (data: RegisterBodyType) => {
+    console.log(data)
+  }
+
+  return (
+    <Form {...form}>
+      <form noValidate className='login-form mx-auto w-full p-6' onSubmit={form.handleSubmit(onSubmit)}>
+        <div className='grid grid-cols-1 gap-6'>
+          <FormField
+            control={form.control}
+            name='full_name'
+            render={({ field }) => (
+              <FormItem>
+                <div className='grid gap-2'>
+                  <Label htmlFor='full_name' className='text-[16px] font-medium'>
+                    Full name<sup aria-hidden='true'>*</sup>
+                  </Label>
+                  <Input
+                    id='full_name'
+                    type='full_name'
+                    placeholder='John Doe'
+                    required
+                    {...field}
+                    className='px-[15px] py-[23px] border-black'
+                  />
+                  <FormMessage />
+                </div>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='email'
+            render={({ field }) => (
+              <FormItem>
+                <div className='grid gap-2'>
+                  <Label htmlFor='email' className='text-[16px] font-medium'>
+                    Email<sup aria-hidden='true'>*</sup>
+                  </Label>
+                  <Input
+                    id='email'
+                    type='email'
+                    placeholder='example@gmail.com'
+                    required
+                    {...field}
+                    className='px-[15px] py-[23px] border-black'
+                  />
+                  <FormMessage />
+                </div>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='password'
+            render={({ field }) => (
+              <FormItem>
+                <div className='grid gap-2'>
+                  <Label htmlFor='password' className='text-[16px] font-medium'>
+                    Password<sup aria-hidden='true'>*</sup>
+                  </Label>
+                  <Input
+                    id='password'
+                    type='password'
+                    placeholder='********'
+                    required
+                    {...field}
+                    className='px-[15px] py-[23px] border-black'
+                  />
+                  <FormMessage />
+                </div>
+              </FormItem>
+            )}
+          />
+          <div className='flex justify-between'>
+            <FormField
+              control={form.control}
+              name='gender'
+              render={({ field }) => (
+                <FormItem>
+                  <div className='grid gap-2'>
+                    <Label htmlFor='gender' className='text-[16px] font-medium'>
+                      Gender<sup aria-hidden='true'>*</sup>
+                    </Label>
+                    <Select onValueChange={field.onChange} value={field.value}>
+                      <SelectTrigger className='w-[180px]'>
+                        <SelectValue placeholder='Select gender' />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value='1'>Male</SelectItem>
+                        <SelectItem value='0'>Female</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </div>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='date_of_birth'
+              render={({ field }) => (
+                <FormItem>
+                  <div className='grid gap-2'>
+                    <Label htmlFor='date_of_birth' className='text-[16px] font-medium'>
+                      Date of birth<sup aria-hidden='true'>*</sup>
+                    </Label>
+                    <DatePicker
+                      endYear={new Date().getFullYear()}
+                      value={field.value ? new Date(field.value) : undefined}
+                      onDateChange={(date) => {
+                        const formattedDate = format(date, 'yyyy-MM-dd')
+                        field.onChange(formattedDate)
+                      }}
+                    />
+                    <FormMessage />
+                  </div>
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormField
+            control={form.control}
+            name='phone_number'
+            render={({ field }) => (
+              <FormItem>
+                <div className='grid gap-2'>
+                  <Label htmlFor='phone_number' className='text-[16px] font-medium'>
+                    Phone number<sup aria-hidden='true'>*</sup>
+                  </Label>
+                  <Input
+                    id='phone_number'
+                    type='phone_number'
+                    placeholder='0123456789'
+                    required
+                    {...field}
+                    className='px-[15px] py-[23px] border-black'
+                  />
+                  <FormMessage />
+                </div>
+              </FormItem>
+            )}
+          />
+          <div className='text-center space-y-4'>
+            <Button type='submit' className='btn login-btn min-w-[150px]'>
+              Register
+            </Button>
+            <p className='text-sm'>
+              Already Have An Account?{' '}
+              <a
+                href={path.login}
+                className='text-[#7c34c8] underline font-bold hover:text-blue-500 transition-all duration-500 ease-in-out'
+              >
+                Login
+              </a>
+            </p>
+          </div>
+        </div>
+      </form>
+    </Form>
+  )
+}
--- src/schema/auth.schema.ts
+++ src/schema/auth.schema.ts
@@ -1,4 +1,50 @@
+import { Role } from '@/constants/type'
 import { z } from 'zod'
+
+export const RegisterBody = z
+  .object({
+    full_name: z.string().trim().min(2, 'Tên phải nhiều hơn 1 ký tự').max(256, 'Tên tối đa 256 ký tự'),
+    email: z.string().email('Email không hợp lệ'),
+    password: z.string().min(6, 'Mật khẩu phải nhiều hơn 5 ký tự').max(100, 'Mật khẩu tối đa 100 ký tự'),
+    gender: z
+      .string()
+      .optional()
+      .refine((val) => val === '0' || val === '1', {
+        message: 'Giới tính phải là Nam hoặc Nữ'
+      }),
+    date_of_birth: z
+      .string()
+      .optional()
+      .refine(
+        (val) => {
+          if (!val) return true
+          const date = new Date(val)
+          return !isNaN(date.getTime())
+        },
+        {
+          message: 'Ngày sinh không hợp lệ'
+        }
+      ),
+    phone_number: z
+      .string()
+      .optional()
+      .refine((val) => /^\d{10}$/.test(val ?? ''), {
+        message: 'Số điện thoại phải có đúng 10 chữ số'
+      })
+  })
+  .strict()
+
+export type RegisterBodyType = z.TypeOf<typeof RegisterBody>
+
+export const RegisterRes = z.object({
+  data: z.object({
+    accessToken: z.string(),
+    refreshToken: z.string()
+  }),
+  message: z.string()
+})
+
+export type RegisterResType = z.TypeOf<typeof RegisterRes>
 
 export const LoginSchema = z
   .object({
